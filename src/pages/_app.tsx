e052c6c842053a5082a23f812d4a6b1c60470ad1
Guilherme Gambini <guilhermegambini5@gmail.com>
2022-09-28 19:12:59 -0300

    logo image now returns to homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 
 import Image from "next/future/image";
+import Link from "next/link";
 
 globalStyles();
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" style={{ cursor: "pointer" }} />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
